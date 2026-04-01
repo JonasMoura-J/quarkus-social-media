28f17288002d0feaa848b67ecc1c3055f73cd635
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-01 12:00:28 -0300

    Adicionando Get

--- src/main/java/io/github/jonas/rest/UserResource.java
+++ src/main/java/io/github/jonas/rest/UserResource.java
@@ -1,15 +1,22 @@
 package io.github.jonas.rest;
 
 import io.github.jonas.dto.CreateUserRequest;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response createUser(CreateUserRequest user){
+        return Response.ok(user).build();
+    }
+
+    @GET
+    public Response listAllUsers(){
         return Response.ok().build();
     }
 }
