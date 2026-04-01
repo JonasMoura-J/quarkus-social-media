8b192a517789f7692d76699113d8bd0ad30bb44c
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-01 17:19:48 -0300

    adicionando validação com hibernate

--- src/main/java/io/github/jonas/rest/UserResource.java
+++ src/main/java/io/github/jonas/rest/UserResource.java
@@ -6,9 +6,13 @@ import io.github.jonas.rest.dto.CreateUserRequest;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.Set;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -16,15 +20,26 @@ import jakarta.ws.rs.core.Response;
 public class UserResource {
 
     private final UserRepository repository;
+    private Validator validator;
 
     @Inject
-    public UserResource(UserRepository repository){
+    public UserResource(UserRepository repository, Validator validator){
         this.repository = repository;
+        this.validator = validator;
     }
 
     @POST
     @Transactional
     public Response createUser(CreateUserRequest userRequest){
+
+        Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
+
+        if(!violations.isEmpty()){
+            ConstraintViolation<CreateUserRequest> err =  violations.stream().findAny().get();
+
+            return Response.status(Response.Status.BAD_REQUEST).entity(err.getMessage()).build();
+        }
+
         User user = new User(userRequest.getName(), userRequest.getAge());
         repository.persist(user);
 
--- src/main/java/io/github/jonas/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/jonas/rest/dto/CreateUserRequest.java
@@ -1,11 +1,14 @@
 package io.github.jonas.rest.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class CreateUserRequest {
 
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotNull(message = "Age is required")
     private Integer age;
-
-
 
     public String getName() {
         return name;
