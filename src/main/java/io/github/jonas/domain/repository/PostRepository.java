f0e504190bf7bc924de5bc6d0553c46b44442cbb
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-03 21:40:51 -0300

    Ajuste do repository

--- src/main/java/io/github/jonas/domain/repository/PostRepository.java
+++ src/main/java/io/github/jonas/domain/repository/PostRepository.java
@@ -1,8 +1,9 @@
 package io.github.jonas.domain.repository;
 
 import io.github.jonas.domain.model.Post;
-import io.github.jonas.domain.model.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class PostRepository implements PanacheRepository<Post> {
 }
